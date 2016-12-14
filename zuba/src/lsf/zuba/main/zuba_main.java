1436e808f7a0e13b147ab8cc9f019859ac3851ed
Anushka <anu.udb@gmail.com>
2016-12-14 08:37:50 +0530

    Added SQL query extractor

--- zuba/src/lsf/zuba/main/zuba_main.java
+++ zuba/src/lsf/zuba/main/zuba_main.java
@@ -16,6 +16,12 @@ public class zuba_main {
 	            ChartParameter chart_obj = new SyntaxChecker(new java.io.StringReader(zuba_query)).Visualization();
 	            System.out.println("Syntax is okay");
 	            System.out.println("result : "+chart_obj.getBindTo());
+
+	            //New code segment for extract the SQL part
+				String sql = zuba_query.substring(zuba_query.indexOf("{")+1,zuba_query.indexOf("}"));
+				System.out.println("SQL: " +sql);
+				
+	            //System.out.println("result : "+chart_obj.getX().getData_parameter());
 	        } catch (Throwable e) {
 	            // Catching Throwable is ugly but JavaCC throws Error objects!
 	            System.out.println("Syntax check failed: " + e.getMessage());
